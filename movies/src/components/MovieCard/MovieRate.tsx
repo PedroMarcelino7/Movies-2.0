6f1dd975a725f1da4b81becd5e928749da9452ce
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-07-28 15:43:48 -0300

    Creating props and mapping movie cards

--- movies/src/components/MovieCard/MovieRate.tsx
+++ movies/src/components/MovieCard/MovieRate.tsx
@@ -1,12 +1,16 @@
 import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
 
-export default function MovieRate() {
+interface Props{
+    rating: number
+}
+
+export default function MovieRate({ rating }: Props) {
     // const [value, setValue] = React.useState<number | null>(2);
 
     return (
         <Box>
-            <Rating name="half-rating-read" value={3.5} precision={0.5} readOnly />
+            <Rating name="half-rating-read" value={rating} precision={0.5} readOnly />
         </Box>
     );
 }
--- movies/src/pages/Reviews/Reviews.tsx
+++ movies/src/pages/Reviews/Reviews.tsx
@@ -1,22 +1,57 @@
 import Container from '@mui/material/Container';
 import { Grid } from '@mui/material';
-
-import MovieCard from '../../components/MovieCard/MovieCard'
+import MovieCard from '../../components/MovieCard/MovieCard';
 
 interface Props {
-    handleOpenEditReview: () => void
+    handleOpenEditReview: () => void;
 }
+
+interface Review {
+    title: string;
+    date: string;
+    img: string;
+    rating: number;
+    review: string;
+}
+
+const reviews: Review[] = [
+    {
+        title: 'Title 1',
+        date: '2024-07-28',
+        img: '',
+        rating: 4,
+        review: ''
+    },
+    {
+        title: 'Title 2',
+        date: '2020-05-20',
+        img: '',
+        rating: 3.5,
+        review: ''
+    },
+    {
+        title: 'Title 3',
+        date: '2018-02-10',
+        img: '',
+        rating: 4.5,
+        review: ''
+    },
+    {
+        title: 'Title 4',
+        date: '2019-01-15',
+        img: '',
+        rating: 5,
+        review: ''
+    },
+];
 
 export default function Reviews({ handleOpenEditReview }: Props) {
     return (
         <Container>
             <Grid container spacing={2}>
-                <MovieCard handleOpenEditReview={handleOpenEditReview} />
-                <MovieCard handleOpenEditReview={handleOpenEditReview} />
-                <MovieCard handleOpenEditReview={handleOpenEditReview} />
-                <MovieCard handleOpenEditReview={handleOpenEditReview} />
-                <MovieCard handleOpenEditReview={handleOpenEditReview} />
-                <MovieCard handleOpenEditReview={handleOpenEditReview} />
+                {reviews.map((review, index) => (
+                    <MovieCard key={index} review={review} handleOpenEditReview={handleOpenEditReview} />
+                ))}
             </Grid>
         </Container>
     );
