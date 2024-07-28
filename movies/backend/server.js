93858c9b782068e89193c63d8f61e464faa94037
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-07-28 20:59:49 -0300

    Setting up backend

---
+++ movies/backend/server.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import cors from 'cors';
+import { connection } from './db.js';
+
+const app = express();
+const SECRET_KEY = 'logged';
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+}));
+
+app.use(express.json());
+
+app.get('/users', (req, res) => {
+    connection.query('SELECT * FROM USERS', (err, results) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.json(results);
+    });
+});
+
+app.post('/users', (req, res) => {
+    const { userEmail, userPassword } = req.body;
+    const query = `
+    INSERT INTO USERS (USER_EMAIL, USER_PASSWORD)
+    VALUES (?, ?)`;
+    const values = [userEmail, userPassword];
+
+    connection.query(query, values, (err, results) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+
+        res.status(201).json(results);
+    });
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
